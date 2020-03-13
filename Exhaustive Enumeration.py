0b01c772d497512d504e30c39d4569769e05ea1f
Rajwol <CupidOfDeath>
2020-03-13 13:32:32 +0545

    Added 3 new algorithms

--- Exhaustive Enumeration.py
+++ Exhaustive Enumeration.py
@@ -1,12 +1,13 @@
-#Finding the cube root of a given number using Newton-Raphson algorithm
+#Finding the square root of a number using exhaustive enumeration
 
 x = int(input('Enter the number which you want to find the square root of: '))
+g = 0.0
 epsilon = 0.01
-g = x / 2
+step = 0.1
 n = 0
 
-while abs(g*g-x) >= epsilon:
-	g = (g-(g*g-x)/(2*g))
+while abs(g*g-x) > epsilon:
+	g += step
 	n += 1
 
 print('The square root of', x, 'is', g)
